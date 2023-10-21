ea647f74edcf43588af0ae0fb17c4a00c429d945
cipher <elisaherter07@gmail.com>
2023-10-20 20:29:28 -0700

    commit

--- 3-1.c
+++ 3-1.c
@@ -1,2 +1,123 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
 
+struct IntArray {
+    int length;
+    int *dataPtr;
+};
+
+struct IntArray* mallocIntArray(int length) {
+    struct IntArray *array = (struct IntArray*)malloc(sizeof(struct IntArray));
+    if (array == NULL) {
+        perror("mallocIntArray");
+        exit(1);
+    }
+    array->length = length;
+    array->dataPtr = (int*)malloc(length * sizeof(int));
+    if (array->dataPtr == NULL) {
+        perror("malloc");
+        exit(1);
+    }
+    return array;
+}
+
+void freeIntArray(struct IntArray *arrayPtr) {
+    free(arrayPtr->dataPtr);
+    free(arrayPtr);
+}
+
+void readIntArray(struct IntArray *array) {
+    printf("Enter %d positive integers:\n", array->length);
+    for (int i = 0; i < array->length; i++) {
+        char input[20];
+        bool validInput = false;
+        while (!validInput) {
+            printf("Enter int: ");
+            if (fgets(input, sizeof(input), stdin) != NULL) {
+                char *endptr;
+                long num = strtol(input, &endptr, 10);
+                if (*endptr == '\n' || *endptr == '\0' && endptr != input) {
+                    if (num > 0) {
+                        array->dataPtr[i] = (int)num;
+                        validInput = true;
+                    } else {
+                        printf("Invalid input. Enter a positive int.\n");
+                    }
+                } else {
+                    printf("Invalid input. Enter a valid int.\n");
+                }
+            } else {
+                printf("Error reading input.\n");
+                exit(1);
+            }
+        }
+    }
+}
+
+void swap(int *xp, int *yp) {
+    int temp = *xp;
+    *xp = *yp;
+    *yp = temp;
+}
+
+void sortIntArray(struct IntArray *array) {
+    for (int i = 0; i < array->length - 1; i++) {
+        for (int j = 0; j < array->length - i - 1; j++) {
+            if (array->dataPtr[j] > array->dataPtr[j + 1]) {
+                swap(&array->dataPtr[j], &array->dataPtr[j + 1]);
+            }
+        }
+    }
+}
+
+void printIntArray(struct IntArray *array) {
+    printf("[");
+    for (int i = 0; i < array->length; i++) {
+        printf(" %d", array->dataPtr[i]);
+        if (i < array->length - 1) {
+            printf(",");
+        }
+    }
+    printf(" ]\n");
+}
+
+int readPositiveInt() {
+    int num;
+    char input[20];
+    bool validInput = false;
+
+    while (!validInput) {
+        printf("Enter an integer: ");
+        if (fgets(input, sizeof(input), stdin) != NULL) {
+            char *endptr;
+            long val = strtol(input, &endptr, 10);
+            if ((*endptr == '\n' || *endptr == '\0') && endptr != input) {
+                num = (int)val;
+                if (num > 0) {
+                    validInput = true;
+                } else {
+                    printf("Invalid input. Enter a positive int.\n");
+                }
+            } else {
+                printf("Invalid input.\n");
+            }
+        } else {
+            printf("Error reading input.\n");
+            exit(1);
+        }
+    }
+    return num;
+}
+
+int main() {
+    int length = readPositiveInt();
+
+    struct IntArray *array = mallocIntArray(length);
+    readIntArray(array);
+    sortIntArray(array);
+    printIntArray(array);
+    freeIntArray(array);
+
+    return 0;
+}
--- 3-2.c
+++ 3-2.c
@@ -1,3 +1,32 @@
 #include <stdlib.h>
 #include <stdio.h>
+//x is in %rdi
+//y is in %rsi
+//z is in %rdx, and that %rax
 
+//subq    %rsi, %rdx
+//imulq   %rdx, %rdi
+//salq    $63, %rdx
+//sarq    $63, %rdx
+//movq    %rdi, %rax
+//orq     %rdx, %rax
+//ret
+
+
+
+int main(int argc, char *argv[]) {
+    if (argc == 4) {
+        long x = strtol(argv[1], NULL, 10);
+        long y = strtol(argv[2], NULL, 10);
+        long z = strtol(argv[3], NULL, 10);
+        printf("f(%ld, %ld, %ld): %ld\n", x, y, z, f(x, y, z));
+    } else {
+        // TODO - your test cases go here!
+        printf("f(1, 2, 4): %ld\n", f(1, 2, 4));
+        printf("f(3, 5, 7): %ld\n", f(3, 5, 7));
+        printf("f(10, 20, 40): %ld\n", f(10, 20, 40));
+        printf("f(30, 50, 70): %ld\n", f(30, 50, 70));
+        printf("f(30, 50, -70): %ld\n", f(30, 50, -70));
+    }
+    return 0;
+}
